f749a7f837e2ea6e1e9e12df248f0391ba3553e0
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-16 21:03:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // add solution here
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay(musicians, instruments) {
-
+    var killingIt = []
 }
